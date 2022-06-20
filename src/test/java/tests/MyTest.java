b8542b3b344392e011159f8465fc17befe3ac019
user <stel-vd@yandex.ru>
2022-06-20 03:53:39 +0300

    Added @Tag("android")

--- src/test/java/tests/MyTest.java
+++ src/test/java/tests/MyTest.java
@@ -4,6 +4,7 @@ package tests;
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -11,6 +12,8 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class MyTest extends TestBase {
+
+    @Tag("android")
     @Test
     void searchTest() {
         String value = "Italy";
