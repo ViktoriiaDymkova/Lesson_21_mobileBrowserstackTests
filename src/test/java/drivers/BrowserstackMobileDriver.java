293d0aa0d28ceedbd03f9e385cd613831af1eafe
user <stel-vd@yandex.ru>
2022-06-20 03:15:49 +0300

    Added credentials.properties

--- src/test/java/drivers/BrowserstackMobileDriver.java
+++ src/test/java/drivers/BrowserstackMobileDriver.java
@@ -1,6 +1,8 @@
 package drivers;
 
 import com.codeborne.selenide.WebDriverProvider;
+import config.CredentialsConfig;
+import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
@@ -10,36 +12,29 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class BrowserstackMobileDriver implements WebDriverProvider {
-/*
- "server": "hub-cloud.browserstack.com",
-    "username": "bsuser_zB7OhS",
-    "access_key": "yjW2gs4QfhiTDpfJZgyk",
-    "capabilities": {
-      "project": "First Junit5 Android Project",
-      "build": "Junit5 Android First",
-      "name": "first_test",
-      "browserstack.debug": true,
-      "app": "bs://ee714901a6bef9d1429d8afe111b9342ea5bd996"
-    },
-    "environments": [{
-      "device": "Google Pixel 3",
-      "os_version":  "9.0"
- */
+
+    static CredentialsConfig credentialsConfig = ConfigFactory.create(CredentialsConfig.class);
+    static String username = credentialsConfig.username();
+    static String access_key = credentialsConfig.access_key();
+    static String app = credentialsConfig.app();
+    static String device = credentialsConfig.device();
+    static String os_version = credentialsConfig.os_version();
+
     @Override
     public WebDriver createDriver(Capabilities capabilities) {
         MutableCapabilities mutableCapabilities = new MutableCapabilities();
         mutableCapabilities.merge(capabilities);
         // Set your access credentials
-        mutableCapabilities.setCapability("browserstack.user", "bsuser_zB7OhS");
-        mutableCapabilities.setCapability("browserstack.key", "yjW2gs4QfhiTDpfJZgyk");
+        mutableCapabilities.setCapability("browserstack.user", username);
+        mutableCapabilities.setCapability("browserstack.key", access_key);
 
         // Set URL of the application under test
 //        mutableCapabilities.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
-        mutableCapabilities.setCapability("app", "bs://ee714901a6bef9d1429d8afe111b9342ea5bd996");
+        mutableCapabilities.setCapability("app", app);
 
         // Specify device and os_version for testing
-        mutableCapabilities.setCapability("device", "Google Pixel 3");
-        mutableCapabilities.setCapability("os_version", "9.0");
+        mutableCapabilities.setCapability("device", device);
+        mutableCapabilities.setCapability("os_version", os_version);
 
         // Set other BrowserStack capabilities
         mutableCapabilities.setCapability("project", "QA.GURU lesson 12/21");
